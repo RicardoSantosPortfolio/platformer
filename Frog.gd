0e9c950e538301339eae3f6983f5267a26acf138
Ricardo Santos <ricardosantos.gamedev@gmail.com>
2024-11-03 15:56:04 -0300

    First commit

---
+++ Frog.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
+var chase = false
+var SPEED = 50
+
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	if chase:\
+		if get_node("AnimatedSprite2D").animation != "Death":
+		get_node("AnimatedSprite2D").play("Jump")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	else:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+	move_and_slide()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		death()
+
+
+func _on_player_collision_body_entered(body):
+	if body.name == "Player":
+		body.health -= 3
+		death()
+		
+func death():
+	velocity.x = 0
+	chase = false
+	get_node("AnimatedSprite2D").play("Death")
+	await get_node("AnimatedSprite2D").animation_finished
+	self.queue_free()
